61ca5842fc373982e441f546e8c5410f3fffc063
oluwasegunyu <charlay_yu@163.com>
2018-05-04 15:08:31 +0800

    添加flask-script

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_script import Manager
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
 if __name__=='__main__':
-    app.run(debug=True)
+    manager.run()
