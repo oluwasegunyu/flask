5a0febf509097783a59585236cd75ee39802f360
oluwasegunyu <charlay_yu@163.com>
2018-05-04 16:22:48 +0800

    添加错误页面，以及模板继承

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 app = Flask(__name__)
-manager = Manager(app)
 bootstrap = Bootstrap(app)
+manager = Manager(app)
+
 
 @app.route('/')
 def index():
@@ -12,6 +13,13 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__=='__main__':
     manager.run()
